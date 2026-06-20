772837fbf22a65c31a6a9ff83e0ede675427ec0f
Mohamad Al-Mozzen <moalm6956@uib.no>
2026-06-20 21:59:03 +0200

    - changes after data parsing rework

--- src/utils/dataloading.py
+++ src/utils/dataloading.py
@@ -1,5 +1,7 @@
+import os
 import torch
 from torch.utils.data import Dataset
+from PIL import Image
 
 class ClipDataset(Dataset):
     def __init__(self, files_list, transform):
@@ -10,10 +12,19 @@ class ClipDataset(Dataset):
         return len(self.files)
 
     def __getitem__(self, key):
-        sample = torch.load(self.files[key])
+        sample = self.files[key]
+        img = Image.open(sample["frames"])
 
-        frames = self.transform(sample["frames"])
+        frames = self.transform(img)
         person = torch.tensor(sample["person"], dtype=torch.float32)
-        age = torch.tensor(sample["age"], dtype=torch.float32)
+        age    = torch.tensor(sample["age"], dtype=torch.float32)
 
         return frames, (person, age)
+    
+class ImageToClip:
+    def __init__(self, transform, n=8):
+        self.transform = transform
+        self.n = n
+
+    def __call__(self, imgTensor):
+        return torch.stack([self.transform(imgTensor) for _ in range(self.n)])
